367baa4a0550a4003c4e587ed7c29b6227a3549e
Grace <melchiorsgrace@gmail.com>
2024-11-14 11:47:33 +0100

    Removed SMAcc

--- SMAcc/SMAcc_Tools.cuh
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "common/sim_config.h"
-#include "../simulation/simulation_config.h"
-#include "results/result_store.h"
-#include  "results/output_write.h"
-
-void convert_config(sim_config* config, const simulation_config* in_config);
--- SMAcc/common/io_paths.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <string>
-
-struct io_paths
-{
-    std::string model_path;
-    std::string output_path;
-    std::string query;
-};
